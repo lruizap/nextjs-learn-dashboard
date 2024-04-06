cb356befe8f71266c5f1497cc5e9d07f7efa5a8e
lruizap <lruizap2502@gmail.com>
2024-04-06 17:48:10 +0200

    ℹ️ Optimized card components 🃏

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -55,6 +55,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
