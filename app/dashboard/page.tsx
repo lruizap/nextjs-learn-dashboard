08e5f37c864df16fc8b84ac2dd1b9f65562e330f
lruizap <lruizap2502@gmail.com>
2024-04-06 16:19:40 +0200

    ℹ️ Connected to the database, installed database extension library, and fetched initial data 🚀

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,10 @@
-export default function DashboardPage() {
+// Esto es un componente que se renderiza en el servidor
+
+import { fetchRevenue } from '../lib/data';
+
+export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+  console.log(revenue);
+
   return <p>Dashboard Page</p>;
 }
